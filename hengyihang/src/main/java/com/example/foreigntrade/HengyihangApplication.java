3f9ece44bb33ba05cdeb2f366dd7096b865451cc
liguanghao <lgh@2022>
2023-04-04 10:52:36 +0800

    hengyihang

---
+++ hengyihang/src/main/java/com/example/foreigntrade/HengyihangApplication.java
@@ -0,0 +1,13 @@
+package com.example.foreigntrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengyihangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HengyihangApplication.class, args);
+    }
+
+}
---
+++ hengyihang/src/test/java/com/example/foreigntrade/HengyihangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foreigntrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HengyihangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
